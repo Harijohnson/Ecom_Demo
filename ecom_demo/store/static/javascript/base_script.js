753c31461d0e748204744e1658e639459cadea38
Harijohnson <harijn72@gmail.com>
2023-10-29 13:08:39 +0530

    base view is created in django appliaction

---
+++ ecom_demo/store/static/javascript/base_script.js
@@ -0,0 +1,11 @@
+function showSidebar(){
+    const sidebar= document.querySelector('.sidebar');
+    sidebar.style.display ='flex';
+
+}
+
+function closeMenuBar(){
+    const sidebar= document.querySelector('.sidebar');
+    sidebar.style.display ='none';
+    
+}
--- ecom_demo/store/views.py
+++ ecom_demo/store/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+# from django.http import 
 
 # Create your views here.
+def store(request):
+    context={}
+    return render(request,"store/store.html",context=context)
