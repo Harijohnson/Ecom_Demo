683fa1482debda38a2b211f7129ab6ff65b9ab27
Harijohnson <harijn72@gmail.com>
2023-10-30 15:59:28 +0530

    base functionalites and web responsivens are working good in laptop

--- ecom_demo/store/static/javascript/base_script.js
+++ ecom_demo/store/static/javascript/base_script.js
@@ -16,8 +16,27 @@ window.addEventListener('scroll', function(){
     header.classList.toggle('sticky',this.window.scrollY>0);
 })
 
+let menu = document.querySelector("#menu-icon");
+let navmenu= document.querySelector(".navmenu");
+
+menu.onclick = () =>{
+    menu.classList.toggle('bx-x');
+    navmenu.classList.toggle('open');
+}
 
 
+function shoeSmallMenu(){
+    const samllMenu = document.querySelector('#menu-icon');
+    samllMenu.style.display ='flex';
+
+}
+
+
+function hideSmallMenu(){
+    const samllMenu = document.querySelector('#menu-icon');
+    samllMenu.style.display ='none';
+
+}
 
 
 
