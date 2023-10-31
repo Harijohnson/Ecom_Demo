b564362efdabc54afe980540d108d82a3cc25622
Harijohnson <harijn72@gmail.com>
2023-10-31 13:34:18 +0530

    logi lots of bugs in login views.py

--- ecom_demo/store/static/javascript/store.js
+++ ecom_demo/store/static/javascript/store.js
@@ -5,47 +5,88 @@ let nameField=document.querySelector('#nameField');
 let forgotPass=document.querySelector('.forgotPass');
 let conBtn = document.querySelector('#con-btn');
 
-const username= document.querySelector('#username').value;
-const email = document.querySelector('#email').value;
-const password = document.querySelector('#password').value;
-const cPass = document.querySelector('#c-password').value;
+
+    // let username= document.querySelector('#username').value;
+    // let email = document.querySelector('#email').value;
+    // let password = document.querySelector('#password').value;
+    // let cPass = document.querySelector('#c-password').value;
+
+    
+//     if (username !== "" && email!== "" && password !== "" && cPass !== "" )
+//     {
+//         signinBtn.type='button';
+//     }    
+//     else{
+//     signupBtn.type='submit';
+//     }
 
 
-console.log(username);
-console.log( email);
-console.log(password);
-console.log(cPass);
+
+// function credChec(){
+//     const email = document.querySelector('#email').value;
+//     const password = document.querySelector('#password').value;
+//     console.log('SIGIN IN ', email);
+//     console.log('SIGIN IN ',password);
+//     if (email!== "" && password !== "")
+//     {
+//         signupBtn.type='buttton';
+//     }    
+//     else{
+//         signinBtn.type='submit';
+//     }
+
+
+// }
+
+
+// second executes
 
 signinBtn.onclick = function() {
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+    
+    console.log('SIGIN IN ', email);
+    console.log('SIGIN IN ',password);
     nameField.style.maxHeight='0';
     title.innerHTML = 'Sign In';
     signupBtn.classList.add('disable');
     signinBtn.classList.remove('disable');
     forgotPass.style.display = 'block';
     conBtn.style.display="none";
-
-    // condition for button and submit
-    if (username !== "" && email!== "" && password !== "" && cPassword !== "" )
+    if (email!== "" && password !== "")
     {
         signinBtn.type='submit';
     }    
     else{
-    signupBtn.type='button';
+        signupBtn.type='button';
+        // location.reload()
     }
 }
+
+
+// first execute
 signupBtn.onclick = function() {
+    let username= document.querySelector('#username').value;
+    let email = document.querySelector('#email').value;
+    let password = document.querySelector('#password').value;
+    let cPass = document.querySelector('#c-password').value;
+
+    console.log('sign up',username, email, password, cPass)
     nameField.style.maxHeight='60px';
     title.innerHTML = 'Sign Up';
     signinBtn.classList.add('disable');
     signupBtn.classList.remove('disable');
     forgotPass.style.display = 'none';
     conBtn.style.display="flex";
-    if (email!== "" && password !== "")
+    
+    if (username !== "" && email!== "" && password !== "" && cPass !== "" )
     {
         signupBtn.type='submit';
     }    
     else{
-        signinBtn.type='button';
+      
+    signinBtn.type='button';
+    
     }
     }
    
--- ecom_demo/store/views.py
+++ ecom_demo/store/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 # from django.http import 
+from django.contrib.auth.models import User   # to update the data into db
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+
+
 
 # Create your views here.
 def store(request):
@@ -11,14 +16,63 @@ def product(request):
     context={}
     return render(request,"store/product_info.html",context=context)
 
+#first action 
+def signup(request):
+    if request.method=='POST':
+        username =  request.POST.get('username')
+        #another way to get the user information from the html
+        # username =  request.POST['username']
+
+        email= request.POST.get('email')
+        password= request.POST.get('password')
+        cPassword= request.POST.get('cPassword')
+
+
+        my_user =User.objects.create_user(username=username, email=email, password=password)
+        # we can add anothe objects as below
+        # my_user.f_name=fname                
+        my_user.save()
+        
+        # to show some information about acount creation 
+        messages.success(request,"Account Created Successfully")
+
+        context=my_user
+        #  redirect to nex html file
+        return render(request,"store/store.html",context=context)
+        # return render(request, 'templates/store/store.html',context=context)
+
+
+
+
+    # context={}
+    # return render(request, 'store/signup.html',context=context)
+
+
+
+
+
 
 def login(request):
-    context={}
-    return render(request, 'store/login.html',context=context)
+    if request.method=='POST':
+        email=request.POST.get('email')
+        password=request.POST.get('password')
 
-def signup(request):
-    context={}
-    return render(request, 'store/signup.html',context=context)
+        user = authenticate(email=email, password=password)
+
+        if user is not None:
+            login(request,user)
+            username:user.username
+            return render(request,"store/login.html",username=username)
+        else:
+            context={}
+            messages.error(request,"you enterd wormg email or password")
+            return render(request,"store/login.html",context=context)
+
+
+
+    # context={}
+    # return render(request, 'store/login.html',context=context)
+
 
 def signout(request):
     context={}
