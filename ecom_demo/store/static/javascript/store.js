54f472caba81fb65fe7eab107e894117fba01b63
Harijohnson <harijn72@gmail.com>
2023-10-31 09:12:06 +0530

    login function bug fixes;

--- ecom_demo/store/static/javascript/store.js
+++ ecom_demo/store/static/javascript/store.js
@@ -3,9 +3,18 @@ let signinBtn= document.querySelector('#signinBtn');
 let title= document.querySelector('#title');
 let nameField=document.querySelector('#nameField');
 let forgotPass=document.querySelector('.forgotPass');
+let conBtn = document.querySelector('#con-btn');
+
+const username= document.querySelector('#username').value;
+const email = document.querySelector('#email').value;
+const password = document.querySelector('#password').value;
+const cPass = document.querySelector('#c-password').value;
 
 
-console.log(forgotPass);
+console.log(username);
+console.log( email);
+console.log(password);
+console.log(cPass);
 
 signinBtn.onclick = function() {
     nameField.style.maxHeight='0';
@@ -13,6 +22,16 @@ signinBtn.onclick = function() {
     signupBtn.classList.add('disable');
     signinBtn.classList.remove('disable');
     forgotPass.style.display = 'block';
+    conBtn.style.display="none";
+
+    // condition for button and submit
+    if (username !== "" && email!== "" && password !== "" && cPassword !== "" )
+    {
+        signinBtn.type='submit';
+    }    
+    else{
+    signupBtn.type='button';
+    }
 }
 signupBtn.onclick = function() {
     nameField.style.maxHeight='60px';
@@ -20,4 +39,15 @@ signupBtn.onclick = function() {
     signinBtn.classList.add('disable');
     signupBtn.classList.remove('disable');
     forgotPass.style.display = 'none';
-}
+    conBtn.style.display="flex";
+    if (email!== "" && password !== "")
+    {
+        signupBtn.type='submit';
+    }    
+    else{
+        signinBtn.type='button';
+    }
+    }
+   
+    
+
